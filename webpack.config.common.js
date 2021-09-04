213eb50c74f78e8ec5e0e0564cb699a52a6ef322
Joseph <JoeSpanbauer@live.com>
2021-09-04 17:30:35 -0400

    Adding server part.

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -26,7 +26,8 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       title: 'TypeScript to OAS 3 Converter',
-      template: path.resolve(__dirname, './src/index.html')
+      template: path.resolve(__dirname, './src/index.html'),
+      filename: "./dist/index.html"
     }),
   ],
   resolve: {
